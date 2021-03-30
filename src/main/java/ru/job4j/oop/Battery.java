a3f437ec81a376268083bf24078314b7faeb773a
Tatyana <anatoliy.gromov.service@gmail.com>
2021-03-30 22:23:38 +0300

    Добавил новый класс Battery.

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+public class Battery {
+
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        this.load += another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery batteryHigh = new Battery(80);
+        Battery batteryLow = new Battery(10);
+        batteryLow.exchange(batteryHigh);
+        System.out.println("batteryHigh load: " + batteryHigh.load);
+        System.out.println("batteryLow load: " + batteryLow.load);
+    }
+}
