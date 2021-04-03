4a1899ee5edfe2082e8d470730ba783e4d891565
Tatyana <anatoliy.gromov.service@gmail.com>
2021-04-03 14:55:02 +0300

    Изменил метод leftShift().

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -3,16 +3,11 @@ package ru.job4j.pojo;
 public class ShopDrop {
 
     public static Product[] leftShift(Product[] products, int index) {
-        for (int i = index; i < products.length; i++) {
-            if (i != products.length - 1) {
-                Product tempProduct = products[i];
+        for (int i = index; i < products.length - 1; i++) {
                 products[i] = products[i + 1];
-                products[i + 1] = tempProduct;
-            } else {
-                products[products.length - 1] = null;
-            }
         }
-        return products;
+            products[products.length - 1] = null;
+            return products;
     }
 
     public static void main(String[] args) {
