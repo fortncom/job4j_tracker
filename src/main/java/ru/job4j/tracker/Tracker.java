72be96df7c1ab03b86c91ffb3889b689d2e5093d
Tatyana <anatoliy.gromov.service@gmail.com>
2021-04-03 17:07:23 +0300

    Исправил метод indexOf().

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -59,7 +59,7 @@ public class Tracker {
     private int indexOf(int id) {
         int rsl = -1;
         for (int index = 0; index < size; index++) {
-            if (items[index] != null && items[index].getId() == id) {
+            if (items[index].getId() == id) {
                 rsl = index;
                 break;
             }
