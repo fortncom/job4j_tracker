e05452cc7ca9eee0e992fcb4428384cf8c9624bc
Tatyana <anatoliy.gromov.service@gmail.com>
2021-04-04 10:15:05 +0300

    Добавил реализацию go().

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,10 +3,16 @@ package ru.job4j.poly;
 public class Bus implements Transport {
 
     private int passengers;
+    private String[] locations = {"Санкт-Петербург", "Казань"};
+    private String currentLocation = locations[0];
 
     @Override
     public void go() {
-
+        if (currentLocation == locations[0]) {
+            System.out.println("Автобус отправляется в город " + locations[1]);
+        } else {
+            System.out.println("Автобус отправляется в город " + locations[0]);
+        }
     }
 
     @Override
