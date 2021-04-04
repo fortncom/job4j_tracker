ffd0442cfff49b7871b67b1c40c660b3b90d6e6e
Tatyana <anatoliy.gromov.service@gmail.com>
2021-04-04 20:33:20 +0300

    Исправил проверку валидности.

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -16,7 +16,7 @@ public class UserStore {
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-            if (!user.isValid() && user.getUsername().length() < 3) {
+            if (!user.isValid() || user.getUsername().length() < 3) {
                 throw new UserInvalidException("Login is not valid or "
                         + "login length less than 3 character.");
             }
