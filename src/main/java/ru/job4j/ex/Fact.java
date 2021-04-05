01e84fc0eafe634c5e8e2954ea35ddba4fb6628c
Tatyana <anatoliy.gromov.service@gmail.com>
2021-04-05 19:47:16 +0300

    Исправил комментарий к исключению.

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -9,7 +9,7 @@ public class Fact {
     public int calc(int n) {
         int rsl = 1;
         if (n < 0) {
-            throw new IllegalStateException("Can't calculate factorial from negative number.");
+            throw new IllegalStateException("Argument must be greater than or equal to zero.");
         }
         for (int index = 1; index <= n; index++) {
             rsl *= index;
