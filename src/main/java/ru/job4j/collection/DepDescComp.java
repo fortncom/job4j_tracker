512e55de48411a9b6380f97d8fb6efb5703508b6
Tatyana <anatoliy.gromov.service@gmail.com>
2021-04-14 21:11:08 +0300

    Убрал лишние условия.

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -1,6 +1,5 @@
 package ru.job4j.collection;
 
-import java.util.Arrays;
 import java.util.Comparator;
 
 public class DepDescComp implements Comparator<String> {
@@ -11,15 +10,7 @@ public class DepDescComp implements Comparator<String> {
         String[] rightDep = right.split("/");
         int rsl = rightDep[0].compareTo(leftDep[0]);
         if (rsl == 0) {
-            for (int i = 1; i < Math.min(leftDep.length, rightDep.length); i++) {
-                rsl = leftDep[i].compareTo(rightDep[i]);
-                if (rsl != 0) {
-                    break;
-                }
-            }
-            if (leftDep.length == 1 || rightDep.length == 1) {
-                rsl = Integer.compare(leftDep.length, rightDep.length);
-            }
+           rsl = left.compareTo(right);
         }
         return rsl;
     }
