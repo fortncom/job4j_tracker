843d7df05472e0c0bcdd9998e13bc93ffa220462
Tatyana <anatoliy.gromov.service@gmail.com>
2021-04-17 21:41:45 +0300

    0. Stream API [#504188]

---
+++ src/main/java/ru/job4j/stream/StreamFilter.java
@@ -0,0 +1,21 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFilter {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>(List.of(
+                -24,
+                46,
+                32,
+                35,
+                -87
+        ));
+        List<Integer> rsl = numbers.stream().filter(integer -> integer > 0)
+                .collect(Collectors.toList());
+        System.out.println(rsl);
+    }
+}
