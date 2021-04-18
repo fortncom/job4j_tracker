8483d4fc5dd2837ea092a3dcccd1489cd3672d5c
Tatyana <anatoliy.gromov.service@gmail.com>
2021-04-18 14:30:00 +0300

    Убрал тест с эксепшеном

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -31,27 +31,6 @@ public class ProfilesTest {
         assertThat(rsl, is(expected));
     }
 
-    @Test(expected = NullPointerException.class)
-    public void whenAddressEqualsNull() {
-        Address address1 = new Address("Kirov", "Mira", 10, 43);
-        Address address2 = null;
-        Address address3 = new Address("Tver", "Pobedy", 17, 1);
-
-        List<Profile> profiles = List.of(
-                new Profile(address1),
-                new Profile(address2),
-                new Profile(address3)
-        );
-        Profiles prof = new Profiles();
-        List<Address> rsl = prof.collect(profiles);
-
-        List<Address> expected = List.of(
-                address1,
-                address2,
-                address3
-        );
-    }
-
     @Test
     public void whenDuplicateAddress() {
         Address address1 = new Address("Kirov", "Mira", 10, 43);
